ccddb61a3a22a1de6523122931b5afa66b09e9d0
HannahChege <chegehannah45@gmail.com>
2018-08-17 17:28:31 +0300

    updated

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,7 @@ import { UsersService } from '../users.service';
 import { HttpClientModule } from '@angular/common/http';
 
 import { Users } from '../users';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -10,12 +11,26 @@ import { Users } from '../users';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-users: Users[];
-constructor(private usersService:UsersService) {
+users: Users;
+
+constructor(private usersService:UsersService,private http:HttpClient) {
   this.usersService.getUsers()
-   }
+
+
+   };
 
   ngOnInit() {
+    interface ApiResponse{
+      User:string;
+      Images:any;
+      Bio:string;
+    }
+    this.http.get<ApiResponse>("https://api.github.com/users/hannahchege?access_token=505a5a7f88928ee9cb67c8b4405280c1b3a2fa1b").subscribe(data=>{
+      // this.users=new Users(data.User,data.Images,data.Bio)
+
+      console.log(data)
+    })
   }
+
 
 }
