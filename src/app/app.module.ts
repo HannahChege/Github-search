f6b9001d611627473dcfba7edfc85dbd65e07f88
HannahChege <chegehannah45@gmail.com>
2018-08-23 08:16:49 +0300

    Adds separate routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
+import {RoutingModule} from './routing/routing.module'
 
 
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { LandingpageComponent } from './landingpage/landingpage.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RoutingModule
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
