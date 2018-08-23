0f975dc41ae14fceb1a57ac11d32264ef987f0a3
HannahChege <chegehannah45@gmail.com>
2018-08-23 07:39:39 +0300

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import {UsersService   }   from './users.service';
 import { HeaderComponent } from './header/header.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
